16c36ce51a76f4121cbb2921c2e4fe4bfe1add6a
root <root@ip-172-31-85-192.ec2.internal>
2022-07-05 22:46:09 +0000

    Conainer_Detail Script for docker updated

---
+++ docker/container_detail/container_detail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#docker image to print your container details in webpage
+
+echo "<h1> CONTAINER ID: `hostname -s` </h1>" >> /var/www/html/index.html
+echo "<h2> HOST IP : `hostname -I` </h2>" >> /var/www/html/index.html
+echo "***********************************" >> /var/www/html/index.html
+echo "<h4> INFO GENRATED ON : $(date) </h4>" >> /var/www/html/index.html
+
+httpd && bash
